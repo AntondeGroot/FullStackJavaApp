02810087ee6fd9334c3b6b2f077bd3a30198461c
Anton de Groot <aammdegroot@gmail.com>
2023-12-29 16:06:13 +0100

    Change directories

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'FullStackJavaApp' title`, () => {
+  it(`should have the 'ToDo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FullStackJavaApp');
+    expect(app.title).toEqual('ToDo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FullStackJavaApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ToDo');
   });
 });
