f2029abeb010e23646fa542cb2fc895bb3c6e04a
Anton de Groot <aammdegroot@gmail.com>
2024-01-11 21:29:09 +0100

    015 implement Put and Post functionality

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes,withComponentInputBinding()),provideHttpClient()]
+  providers: [provideRouter(routes,withComponentInputBinding()),provideHttpClient(),DatePipe]
 };
--- frontend/src/app/list-todos/list-todos.component.ts
+++ frontend/src/app/list-todos/list-todos.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TodoDataService } from '../service/data/todo-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-todos',
@@ -15,24 +16,32 @@ export class ListTodosComponent {
   message: string = '';
 
   constructor(
-    private todoService: TodoDataService
+    private todoService: TodoDataService,
+    private router: Router
   ) {
     this.todos = [];
+    this.refreshTodos();
+  }
+
+  refreshTodos(){
     this.todoService.retrieveAllTodos('username1').subscribe(
       response => {
         this.todos = response
       }
     );
-
-
   }
 
   deleteTodo(id: number) {
     this.todoService.deleteTodo('username123',id).subscribe(
       response => {
-        this.message = 'Delete Successful!'
+        this.message = 'Delete Successful!';
+        this.refreshTodos();
       }
     );
+  }
+
+  updateTodo(id: number) {
+    this.router.navigate([`todos/${id}`]);
   }
 }
 
